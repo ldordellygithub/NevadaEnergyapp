36e796f880b1d5c16a2751840f3ef5a9527eb4c4
peke18 <jennifervelas13@gmail.com>
2024-06-10 23:08:19 -0500

    primera parte de la clase administration

--- security-service/src/main/java/com/moduloGestionUsuarios/security_service/models/Administration.java
+++ security-service/src/main/java/com/moduloGestionUsuarios/security_service/models/Administration.java
@@ -1,4 +1,29 @@
 package com.moduloGestionUsuarios.security_service.models;
 
-public class Administration {
+public class  Administration{
+    
+    // Atributo privado para la lista de usuarios
+    private String userList;
+    
+    // Este metodo asigna el valor de userList
+    public void setuserList(String userList) {
+        this.userList = userList;
+    }
+    
+    // Este metodo le devuelve el valor a userList
+    public String getuserList (){
+        return userList;
+    }
+    
+    //Implementacion para asignar un rol a un usuario
+    public void assignRole(User user,String role){ 
+    }
+    
+    //Implementacion gestionar un usuario
+    public void manageUser(User user){
+    }
+    
+    //Implementacion para eliminar el usuario
+    public void deleteUser(User user){
+    }
 }
